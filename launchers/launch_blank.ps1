a112961bcff8a9941cce08f012509015bf5844c2
Satish Goda <satishgoda@live.com>
2022-03-11 16:43:12 -0800

    Create launch_blank.ps1
    https://stackoverflow.com/questions/15437244/how-to-pipe-all-output-of-exe-execution-in-powershell

---
+++ launchers/launch_blank.ps1
@@ -0,0 +1,3 @@
+PS ..\code\launchers\b3d\b310> $env:BLENDER_USER_SCRIPTS = ".\scripts\"
+PS ..\code\launchers\b3d\b310> $env:BLENDER_USER_CONFIG = ".\config\"
+PS ..\launchers\b3d\b310> cmd /c "blender.exe" 2`>`&1
